6bda3b5ed694c0ba93eede8e27f5bf2b721ce580
nordland <nordland@csee.ltu.se>
2006-12-05 13:32:38 +0100

    Added comments

--- Syntax2Core.hs
+++ Syntax2Core.hs
@@ -9,19 +9,24 @@ import qualified Core
 
 syntax2core m = s2c m
 
+
+-- translate a module in the empty environment
 s2c                             :: Module -> M Core.Module
 s2c (Module v ds)               = do (ts,pe,bs) <- s2cDecls env0 ds [] [] [] []
                                      return (Core.Module v ts pe bs)
   where env0                    = Env { sigs = [] }
 
 
+-- type signature environments
 data Env                        = Env { sigs :: Map Name Type }
 
 addSigs te env                  = env { sigs = te ++ sigs env }
 
 
--- Syntax to Core translation of declarations -----------------------------------------------
+-- Syntax to Core translation of declarations ========================================================
 
+-- translate top-level declarations, accumulating signature environment env, kind environment ke, 
+-- type declarations ts, predicate environment (instance signatures) pe, as well as bindings bs
 s2cDecls env [] ke ts pe bs     = do (te,bs2) <- s2cBinds (addSigs pe env) te es2
                                      (_ ,bs1) <- s2cBinds (addSigs te env) pe es1
                                      ke' <- mapM s2cKSig (impl_ke `zip` repeat KWild)
@@ -30,7 +35,7 @@ s2cDecls env [] ke ts pe bs     = do (te,bs2) <- s2cBinds (addSigs pe env) te es
   where (te,es)                 = splitBinds (reverse bs)
         (es1,es2)               = partition ((`elem` ws) . fst) es
         impl_ke                 = dom ts \\ dom ke
-        ws                      = dom pe
+        ws                      = doms2cSigpe
 
 
 s2cDecls env (DKSig c k : ds) ke ts pe bs
@@ -55,54 +60,60 @@ s2cDecls env (DPSig w p : ds) ke ts pe bs
 s2cDecls env (DBind b : ds) ke ts pe bs
                                 = s2cDecls env ds ke ts pe (b:bs)
 
-
+--translate a constructor declaration
 s2cConstr (Constr c ts ps)      = do ts <- mapM s2cQualType ts
                                      (qs,ke) <- s2cQuals ps
                                      return (c, Core.Constr ts qs ke)
 
 
+-- translate a field declaration
 s2cSig (Sig vs qt)              = s2cTSig vs qt
 
 
-  
+-- add suppressed wildcard kind  
 pVar v                          = PKind v KWild
 
+
+-- buld a signature environment for data constructors cs in type declaration tc vs
 teConstrs tc vs cs              = map f cs
   where t0                      = foldl TAp (TCon tc) (map TVar vs)
         f (Constr c ts ps)      = (c,TQual (tFun ts t0) (ps ++ map pVar vs))
 
-
+-- build a signature environment for record selectors ss in type declaration tc vs
 teSigs tc vs ss                 = concat (map f ss)
   where t0                      = foldl TAp (TCon tc) (map TVar vs)
         f (Sig ws (TQual t ps)) = ws `zip` repeat (TQual (TFun [t0] t) (ps ++ map pVar vs))
         f (Sig ws t)            = ws `zip` repeat (TQual (TFun [t0] t) (map pVar vs))
 
-
+-- build a signature environment for type class members ss in class declaration tc vs
 teMems tc vs ss                 = concat (map f ss)
   where p0                      = PType (foldl TAp (TCon tc) (map TVar vs))
         f (Sig ws (TQual t ps)) = ws `zip` repeat (TQual t (p0 : ps ++ map pVar vs))
         f (Sig ws t)            = ws `zip` repeat (TQual t (p0 : map pVar vs))
 
 
--- Signatures -----------------------------------------------------------
+-- Signatures =========================================================================
 
+-- translate a type signature
 s2cTSig vs t                    = do ts <- mapM (s2cQualType . const t) vs
                                      return (vs `zip` ts)
 
 
--- Types -----------------------------------------------------------------
+-- Types ==============================================================================
 
+-- translate a qualified type scheme
 s2cQualType (TQual t qs)        = do (ps,ke) <- s2cQuals qs
                                      t <- s2cRhoType t
                                      return (Core.Scheme t ps ke)
 
-
+-- translate a rank-N function type
 s2cRhoType (TFun ts t)          = do ts <- mapM s2cQualType ts
                                      t <- s2cRhoType t
                                      return (Core.F ts t)
 s2cRhoType t                    = liftM Core.R (s2cType t)
 
 
+-- translate a monomorphic type
 s2cType (TFun ts t)             = do ts <- mapM s2cType ts
                                      t <- s2cType t
                                      return (Core.TFun ts t)
@@ -115,6 +126,7 @@ s2cType (TWild)                 = do k <- newKVar
                                      Core.newTVar k
 
 
+-- translate qualifiers, separating predicates from kind signatures along the way
 s2cQuals qs                     = s2c [] [] qs
   where
     s2c ps ke []                = return (reverse ps, reverse ke)
@@ -124,8 +136,9 @@ s2cQuals qs                     = s2c [] [] qs
                                      s2c (p : ps) ke qs
 
 
--- Kinds -----------------------------------------------------------------------------
+-- Kinds ==============================================================================
 
+-- translate a kind
 s2cKind Star                    = return Star
 s2cKind KWild                   = newKVar
 s2cKind (KFun k k')             = liftM2 KFun (s2cKind k) (s2cKind k')
@@ -133,11 +146,13 @@ s2cKind (KFun k k')             = liftM2 KFun (s2cKind k) (s2cKind k')
 s2cKSig (v,k)                   = do k <- s2cKind k
                                      return (v,k)
 
--- Expressions and bindings -----------------------------------------------------------
+-- Expressions and bindings ============================================================
 
+-- the translated default case alternative
 dflt                                    = Core.EVar (prim Fail)
 
 
+-- translate a case alternative, inheriting type signature t top-down
 s2cA env t (Alt (ELit l) (RExp e))      = do e' <- s2cEc env t e
                                              return (Core.PLit l, e')
 s2cA env t (Alt (ECon c) (RExp e))      = do e' <- s2cEc env (TFun ts t) e
@@ -145,12 +160,12 @@ s2cA env t (Alt (ECon c) (RExp e))      = do e' <- s2cEc env (TFun ts t) e
   where ts                              = splitArgs (lookupT c env)
 
 
-
+-- translate a record field
 s2cF env (Field s e)                    = do e <- s2cEc env (snd (splitT (lookupT s env))) e
                                              return (s,e)
 
 
-
+-- split bindings bs into type signatures and equations
 splitBinds bs                           = s2cB [] [] bs
   where 
     s2cB sigs eqs []                    = (reverse sigs, reverse eqs)
@@ -159,6 +174,7 @@ splitBinds bs                           = s2cB [] [] bs
                                         = s2cB sigs ((v,e):eqs) bs
 
 
+-- translate equation eqs in the scope of corresponding signatures sigs
 s2cBinds env sigs eqs                   = do (ts,es) <- fmap unzip (mapM s2cEqn eqs)
                                              let te = vs `zip` ts
                                              te' <- s2cTE te
@@ -171,8 +187,9 @@ s2cBinds env sigs eqs                   = do (ts,es) <- fmap unzip (mapM s2cEqn 
                                                           return (t,e)
 
 
--- Expressions, checking mode -------------------------------------------------------
+-- Expressions, inherit mode ===============================================================
 
+-- translate an expression, inheriting type signature t top-down
 s2cEc env t (ELam ps e)         = do e' <- s2cEc (addSigs te env) t' e
                                      te' <- s2cTE te
                                      return (Core.ELam te' e')
@@ -198,8 +215,10 @@ s2cEc env t (ELit l)            = return (Core.ELit l)
 s2cEc env _ e                   = s2cE env e
 
 
--- Expressions, agnostic mode -----------------------------------------------------
+-- Expressions, agnostic mode ================================================================
 
+-- translate an expression whose type cannot be rank-N polymorphic 
+-- (i.e., no point inheriting nor synthesizing signatures)
 s2cE env (ERec _ fs)                    = do eqs <- mapM (s2cF env) fs
                                              return (Core.ERec eqs)
 s2cE env (EAct x ss)                    = do c <- s2cS env ss
@@ -228,8 +247,9 @@ s2cE env (ETempl x ss)                  = do c <- s2cS (addSigs te env) (map uns
 s2cE env e                              = error ("Internal s2cE " ++ show e)
 
 
--- Statements ----------------------------------------------------------------------
+-- Statements ==================================================================================
 
+-- translate a statement list
 s2cS env []                             = return (Core.CRet (Core.ECon (prim UNIT)))
 s2cS env [SRet e]                       = do (t,e') <- s2cEi env e
                                              return (Core.CRet e')
@@ -250,6 +270,7 @@ s2cS env (SAss (EVar v) e : ss)         = do e' <- s2cEc env (lookupT v env) e
 s2cS env (SBind b : ss)                 = s2cS' env [b] ss
 
 
+-- translate a binding group prefix of a statement list
 s2cS' env bs (SBind b : ss)             = s2cS' env (b:bs) ss
 s2cS' env bs ss                         = do (te',bs') <- s2cBinds env te es
                                              c <- s2cS (addSigs te' env) ss
@@ -257,8 +278,9 @@ s2cS' env bs ss                         = do (te',bs') <- s2cBinds env te es
   where (te,es)                         = splitBinds (reverse bs)
 
 
--- Expressions, inference mode ---------------------------------------------------
+-- Expressions, synthesize mode ================================================================
 
+-- translate an expression, synthesize type signature bottom-up
 s2cEi env (ELam ps e)           = do (t,e') <- s2cEi (addSigs te env) e
                                      te' <- s2cTE te
                                      return (TFun (rng te) t, Core.ELam te' e')
@@ -285,22 +307,28 @@ s2cEi env e                     = do e' <- s2cE env e
                                      return (TWild, e')
 
 
--- Misc ---------------------------------------------------------------------------
+-- Misc ====================================================================================
 
+-- translate type enviroment te
 s2cTE te                        = mapM s2cVT te
   where s2cVT (v,t)             = do t <- s2cQualType t
                                      return (v,t)
 
 
+-- split a function type into domain (one parameter only) and range
+-- if not a function type, fail gracefully (error will be caught later by real type-checker)
 splitT (TFun [t] t')            = (t, t')
 splitT (TFun (t:ts) t')         = (t, TFun ts t')
+splitT TWild                    = (TWild,TWild)
 splitT _                        = (TWild,TWild)
 
 
+-- return the domain of a function type (all immediate parameters)
 splitArgs (TFun ts t)           = ts
 splitArgs t                     = []
 
 
+-- merge any signatures in patterns ps with domain of type t, pair with range of t
 mergeT ps t                     = (zipWith f ts ps, t')
   where (ts,t')                 = split (length ps) t
         f t (EVar v)            = (v,t)
@@ -312,11 +340,13 @@ mergeT ps t                     = (zipWith f ts ps, t')
                 (ts,t')         = split (n-1) t2
 
 
+-- find and instantate the type signature for x if present, otherwise return _
 lookupT x env                   = case lookup x (sigs env) of
                                     Nothing -> TWild
                                     Just t  -> peel t
 
 
+-- peel off the outermost qualifiers of a type, replacing all bound variables with _
 peel (TQual t ps)               = g (bvars ps) t
   where g vs (TQual t ps)       = TQual (g vs' t) (map (h vs') ps)
           where vs'             = vs \\ bvars ps
