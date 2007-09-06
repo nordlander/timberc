9a1f15b839d6ece021b44cd50fd08ab1669ee209
sydow <sydow@chalmers.se>
2007-09-06 22:59:34 +0200

    Added warning for name clash from imported modules

--- Common.hs
+++ Common.hs
@@ -186,7 +186,9 @@ mergeRenamings1 rn1 rn2         = rn1 ++ rn2'
 
 -- for merging renamings from two imported modules;
 -- removes both occurrences when two unqualified names clash
-mergeRenamings2 rn1 rn2         = rn1' ++ rn2'
+mergeRenamings2 rn1 rn2         = case ns of
+                                     [] -> rn1' ++ rn2'
+                                     _  -> tr' ("Warning: clash of imported name(s): "++showids ns++ "\n") (rn1' ++ rn2')
   where (rn2',ns)               = deleteRenamings rn1 rn2
         rn1'                    = deleteNames ns rn1
         deleteNames [] rn       = rn
