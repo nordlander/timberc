5ec230371f83e7a48d423d3a35cd9d08804bd830
nordland <nordland@csee.ltu.se>
2007-10-24 15:03:26 +0200

    A POSIX rts, first steps

--- rtsPOSIX/rts.c
+++ rtsPOSIX/rts.c
@@ -6,39 +6,7 @@
 
 // Memory management --------------------------------------------------------------------------------
 
-#define allocwords(size)   malloc(size*sizeof(WORD))
-#define HEAPSIZE           0x100000
-
-
-ADDR base, lim, hp;
-WORD heapsize, thissize;
-
-
-void panic(void) {
-        fprintf(stderr, "Timber RTS cannot allocate more memory, quitting...\n");
-}
-
-void extend(WORD size) {
-    thissize = (size > HEAPSIZE-1 ? size : HEAPSIZE);
-    ADDR a = (ADDR)allocwords(thissize);
-    if (!a)
-        panic();
-    base[0] = (WORD)a;
-    a[0] = 0;
-    base = a;
-    lim = a + thissize;
-    hp = a + 1;
-    heapsize += thissize;
-}
-
-ADDR force(WORD size) {                 // Heap overflow in from-space...
-    ADDR a;
-    extend(size);
-    a = hp;
-    hp += size;
-    return a;
-}
-
+#include "gc.c"
 
 // Exception handling ----------------------------------------------------------------------------------
 
@@ -50,80 +18,15 @@ POLY RAISE(Int err) {
 
 // String marshalling ----------------------------------------------------------------------------------
 
-void putStr(LIST xs) {
-  Char x;
-  while(1) {
-    switch ((Int)xs) {
-    case _NIL: 
-      putchar('\n');
-      return;
-    default: 
-      x = (Char)(Int)((CONS)xs)->a;
-      xs = ((CONS)xs)->b;
-      putchar(x);
-      break;
-    }
-  }
-}
-
-LIST getStr(char *p) {
-    if (!*p)
-        return (LIST)_NIL;
-    CONS n0; NEW(CONS, n0, sizeof(struct CONS));
-    CONS n = n0;
-    n->a = (POLY)(Int)*p++;
-    while (*p) {
-        NEW(LIST, n->b, sizeof(struct CONS));
-        n = (CONS)n->b;
-        n->a = (POLY)(Int)*p++;
-    }
-    n->b = (LIST)_NIL;
-    return (LIST)n0;
-}
-
+#include "timberstrings.c"
 
 // Arrays ---------------------------------------------------------------------------------------------
 
-Array primListArray(LIST xs) {
-  Int len = 0;
-  Int i = 0;
-  LIST ys = xs;
-  while ((Int)ys) {len++; ys = ((CONS)ys)->b;}; // not nice to compute the length here ...
-  Array res; NEW (Array,res,sizeof(Int)+ len*sizeof(POLY)); 
-  res->size = len;
-  ys = xs;
-  while((Int)ys) {res->elems[i] = ((CONS)ys)->a; ys = ((CONS)ys)->b; i++;}
-  return res;
-}
-
-Array primConstArray(Int len, POLY a) {
-  Int i;
-  Array res; NEW (Array,res,sizeof(Int)+ len*sizeof(POLY)); 
-  res->size = len;
-  for(i=0; i<len; i++) res->elems[i] = a;
-  return res;
-}
-
-Array primCloneArray(Array a, Int lev) {
-  switch (lev) {
-     case 0: return a;
-     default: {
-       Array res; NEW(Array,res,sizeof(Int)+ a->size * sizeof(POLY));
-       res->size = a->size;
-       Int i; for(i=0; i < a->size; i++) res->elems[i] = primCloneArray(a->elems[i],lev-1);
-       return res;
-     }
-  }
-}
-
+#include "arrays.c"
 
 // Initialization -------------------------------------------------------------------------------------
 
 void init_rts(void) {
-    base = (ADDR)allocwords(HEAPSIZE);
-    base[0] = 0;
-    hp = base + 1;
-    lim = base + HEAPSIZE;
-    heapsize = thissize = HEAPSIZE;
+    gcinit();
 }
 
--- rtsPOSIX/rts.h
+++ rtsPOSIX/rts.h
@@ -1,10 +1,22 @@
 #ifndef RTS_H_
 #define RTS_H_
 
-#define WORDS(bytes)      (((bytes)+sizeof(WORD)-1)/sizeof(WORD))
-#define NEW(t,lhs,size)   lhs = (t)hp; hp += WORDS(size); if (hp >= lim) lhs = (t)force(WORDS(size));
+#define WORDS(bytes)        (((bytes)+sizeof(WORD)-1)/sizeof(WORD))
+#define ISBLACK(a)          (a >= base2 && a < scanp)
 
-extern ADDR hp, lim;
+#define GCINFO(addr)        ((ADDR)addr[0])
+#define NEW(t,addr,size)    { DISABLE(); addr = (t)hp; hp += WORDS(size); if (hp >= lim) addr = (t)force(WORDS(size)); ENABLE(); }
+#define NEW2(addr,size)     { DISABLE(); addr = hp2; hp2 += WORDS(size); if (hp2 >= lim2) addr = force2(WORDS(size)); ENABLE(); }
+#define PROLOGUE(obj)       { if (GCINFO(obj)[0] < 0) obj = (ADDR)obj[1]; }
+#define EPILOGUE(obj)       { if (hp2) { \
+                                 if (obj==scanning) scanning = 0; \
+                                 if (obj==copying) copying = 0; \
+                                 if (ISBLACK(obj)) { ADDR a; NEW2(a,1); a[0] = (WORD)obj; } } }     // write barrier
+
+
+extern ADDR hp, lim, hp2, lim2, base2, scanp;
+extern volatile ADDR scanning, copying;
+
 ADDR force(WORD);
 
 UNITTYPE ASYNC(Msg, Time, Time);
