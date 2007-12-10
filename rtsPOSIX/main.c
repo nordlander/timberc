2b98e9b20b82547be5c067988f33f165bb665b4d
sydow <sydow@chalmers.se>
2007-12-10 13:45:58 +0100

    Removed tags from names in main.c

--- rtsPOSIX/main.c
+++ rtsPOSIX/main.c
@@ -3,15 +3,15 @@
 #include "POSIX.h"
 
 void ROOTINIT(void);
-extern Prog_1_POSIX ROOT(Env_2_POSIX);
+extern Prog_POSIX ROOT(Env_POSIX);
 
-extern Prog_1_POSIX prog;
-extern Env_2_POSIX env;
+extern Prog_POSIX prog;
+extern Env_POSIX env;
 
 int main(int argc, char **argv) {
     init_rts(argc, argv);
     ROOTINIT();
     prog = ROOT(env);
-    prog->start_5_POSIX(prog,-1,-1);
+    prog->start_POSIX(prog,-1,-1);
     run();
 }
