49b8132cbbae06fb27ab8b19b6378a7a12452415
nordland <nordland@csee.ltu.se>
2007-12-13 12:17:22 +0100

    Array size adjustment

--- include/arrays.c
+++ include/arrays.c
@@ -7,7 +7,7 @@ Array primListArray(LIST xs) {
   Int i = 0;
   LIST ys = xs;
   while ((Int)ys) {len++; ys = ((CONS)ys)->b;}; // not nice to compute the length here ...
-  Array res; NEW (Array,res,sizeof(Int)+sizeof(void*)+len*sizeof(POLY)); 
+  Array res; NEW (Array,res,sizeof(Array)+len*sizeof(POLY)); 
   res->size = len;
   ys = xs;
   while((Int)ys) {res->elems[i] = ((CONS)ys)->a; ys = ((CONS)ys)->b; i++;}
@@ -16,7 +16,7 @@ Array primListArray(LIST xs) {
 
 Array primConstArray(Int len, POLY a) {
   Int i;
-  Array res; NEW (Array,res,sizeof(Int)+sizeof(void*)+len*sizeof(POLY)); 
+  Array res; NEW (Array,res,sizeof(Array)+len*sizeof(POLY)); 
   res->size = len;
   for(i=0; i<len; i++) res->elems[i] = a;
   return res;
@@ -26,7 +26,7 @@ Array primCloneArray(Array a, Int lev) {
   switch (lev) {
      case 0: return a;
      default: {
-       Array res; NEW(Array,res,sizeof(Int)+sizeof(void*)+a->size * sizeof(POLY));
+       Array res; NEW(Array,res,sizeof(Array)+a->size * sizeof(POLY));
        res->size = a->size;
        Int i; for(i=0; i < a->size; i++) res->elems[i] = primCloneArray(a->elems[i],lev-1);
        return res;
