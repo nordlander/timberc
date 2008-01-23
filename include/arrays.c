62eac3a51f23ab8c0b6f0586a145344a5ac0589c
nordland <nordland@csee.ltu.se>
2008-01-23 13:07:35 +0100

    Yet another byte/word confusion fix

--- include/arrays.c
+++ include/arrays.c
@@ -7,7 +7,7 @@ Array primListArray(LIST xs) {
         Int i = 0;
         LIST ys = xs;
         while ((Int)ys) {len++; ys = ((CONS)ys)->b;}; // not nice to compute the length here ...
-        Array res; NEW (Array,res,sizeof(struct Array)+len*sizeof(POLY));
+        Array res; NEW (Array,res,WORDS(sizeof(struct Array))+len);
         res->size = len;
         ys = xs;
         while((Int)ys) {res->elems[i] = ((CONS)ys)->a; ys = ((CONS)ys)->b; i++;}
@@ -16,14 +16,14 @@ Array primListArray(LIST xs) {
 
 Array primConstArray(Int len, POLY a) {
         Int i;
-        Array res; NEW (Array,res,sizeof(struct Array)+len*sizeof(POLY)); 
+        Array res; NEW (Array,res,WORDS(sizeof(struct Array))+len);
         res->size = len;
         for(i=0; i<len; i++) res->elems[i] = a;
         return res;
 }
 
 Array primEmptyArray(Int len) {
-        Array res; NEW (Array,res,sizeof(struct Array)+len*sizeof(POLY)); 
+        Array res; NEW (Array,res,WORDS(sizeof(struct Array))+len); 
         res->size = len;
         return res;
 }
@@ -32,7 +32,7 @@ Array primCloneArray(Array a, Int lev) {
         if (!lev)
                 return a;
         Int i; 
-        Array res; NEW(Array,res,sizeof(struct Array)+a->size * sizeof(POLY));
+        Array res; NEW(Array,res,WORDS(sizeof(struct Array))+a->size);
         res->size = a->size;
         lev--;
         if (lev) for (i=0; i < a->size; i++) res->elems[i] = primCloneArray(a->elems[i], lev);
