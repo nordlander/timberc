3eaa07105eaf2ba49d86505337471b2e994a07e6
sydow <sydow@chalmers.se>
2008-08-26 15:51:01 +0200

    Changed generated state names to be qualified

--- Type.hs
+++ Type.hs
@@ -234,11 +234,11 @@ tiExp env (EDo x tx c)
                                      return (s'++s, pe, R (tCmd tx t), EDo x tx c)
   | otherwise                   = internalError0 "Explicitly typed do expressions not yet implemented"
 tiExp env (ETempl x tx te c)
-  | isTVar tx                   = do n <- newName stateSym
+  | isTVar tx                   = do n <- newNameMod (modName env) stateSym
                                      let env' = setSelf x (TId n) (addTEnv te (addKEnv0 [(n,Star)] env))
                                      (s,pe,t,c) <- tiCmd env' c
                                      return ((TId n, tx):s, pe, R (tClass t), ETempl x (TId n) te c)
-  | otherwise                   = internalError0 "Explicitly typed template expressions not yet implemented"
+  | otherwise                   = internalError0 "Explicitly typed class expressions not yet implemented"
 
 
 tiCmd env (CRet e)              = do alpha <- newTVar Star
