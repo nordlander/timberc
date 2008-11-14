8e1a0bb2992eff94f6fd84261bd809148c02233d
sydow <sydow@chalmers.se>
2008-11-14 12:41:50 +0100

    Improved warnig message when clash on import

--- Common.hs
+++ Common.hs
@@ -292,7 +292,7 @@ mergeRenamings1 rn1 rn2         = rn1 ++ rn2'
 -- removes both occurrences when two unqualified names clash
 mergeRenamings2 rn1 rn2         = case ns' of
                                      [] -> rn1' ++ rn2'
-                                     _  -> tr' ("Warning: clash of imported name(s): "++showids ns' ++ "\n") (rn1' ++ rn2')
+                                     _  -> tr' ("Warning: clash of imported name(s): "++showids ns' ++ "(hence not in scope)\n") (rn1' ++ rn2')
   where (rn2',ns)               = deleteRenamings rn1 rn2
         rn1'                    = deleteNames ns rn1
         ns'                     = filter (not . isGenerated) ns
