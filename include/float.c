a22829732e5862d11b57100b3024e4b2366ca2ac
Johan Nordlander <johan.nordlander@ltu.se>
2012-01-24 16:32:37 +0100

    Simplified the float-to-string primitive. Should work well on Ubuntu now.

--- include/float.c
+++ include/float.c
@@ -33,21 +33,10 @@
 
 #include "timber.h"
 
-struct DummyStruct {
-  WORD *GCINFO;
-  Int n;
-};
-
-typedef struct DummyStruct *DummyStruct;
-
-static WORD __GC__DummyStruct[] = {WORDS(sizeof(struct DummyStruct)+20), GC_STD, 0};
-
 LIST primShowFloat(Float x) {
-  DummyStruct p;
-  NEW(DummyStruct,p,sizeof(struct DummyStruct)+20);
-  p->GCINFO = __GC__DummyStruct;
-  snprintf((char*)&p->n,20,"%e",x);
-  LIST res = getStr((char*)&p->n);
-  return res;
+    char buf[16];
+    snprintf(buf,16,"%e",x);
+    LIST res = getStr(buf);
+    return res;
 }
  
